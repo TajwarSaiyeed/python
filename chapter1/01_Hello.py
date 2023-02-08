eef49f15c6e0d05cb0a447475948ec5dc93f4d3c
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-08 07:53:07 +0600

    python comments | chapter 1

--- chapter1/01_Hello.py
+++ chapter1/01_Hello.py
@@ -1,5 +1,15 @@
-import os
-import math
 
-print("hello world")
-print(5+3)
+# comment
+# comments are used to write someting which the programmer does not want to execute
+
+
+# eg: print("hello world") # this is a comment
+# author : abid
+# License : abid company
+
+import os  # import os module
+
+''' 
+this is a multiline comment
+This code is written by abid 
+'''
