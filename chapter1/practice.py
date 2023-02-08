5646b37f842eb4e27b9b261eedcb4cf7687ff3ce
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-08 07:57:04 +0600

    update

---
+++ chapter1/practice.py
@@ -0,0 +1,12 @@
+# practice.py
+
+
+print("Twinke, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.\nTwinkle, twinkle, little star,\nHow I wonder what you are\n\n")
+
+
+print('''Twinke, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star
+      ''')
