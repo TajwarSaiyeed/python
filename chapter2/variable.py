a43860c22c896875d7d2bb2df4db27165be71f4d
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-09 21:42:46 +0600

    variable | single code | double code

---
+++ chapter2/variable.py
@@ -0,0 +1,19 @@
+# what is a variable?
+# variable is a container which holds the value of a data type like string, integer, float, boolean etc
+
+singlecode = 'single code abid'
+doublecode = "double code abid"
+triplecode = ''' "this" 'is' a triple code'''
+b = 345
+c = 42.54
+d = True
+e = None
+
+
+print(singlecode)
+print(doublecode)
+print(triplecode)
+print(b)
+print(c)
+print(d)
+print(e)
