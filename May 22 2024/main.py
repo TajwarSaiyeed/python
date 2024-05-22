9e2af3ffcf5368eb474c94d1ceda8b05fc978956
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-22 18:29:00 +0600

    chore : variable, type casting, data types

---
+++ May 22 2024/main.py
@@ -0,0 +1,50 @@
+print("Hello world")
+# print("Hello tajwar saiyeed abid")
+
+
+# single line comment
+
+'''
+This is a multi line comment
+'''
+
+# variables
+
+a = 10
+b = True
+print(a, b)
+
+a = 3.1416
+print(a)
+
+# Data types
+
+# int, float, str, bool
+
+a = 10
+b = 3.1416
+c = "Hello World"
+d = True
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+
+# Type casting
+
+a = 10
+b = 3.1416
+
+print(float(a))
+print(int(b))
+
+e = None
+print(e, type(e))
+
+f = "5"
+print(f, type(f))
+g = 5
+print(g, type(g))
+
+print(int(f) + g)
