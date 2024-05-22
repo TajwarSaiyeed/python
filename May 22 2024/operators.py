cbc13b2cd33249870bfd4d1fc43a7c49418d7ae9
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-22 18:36:22 +0600

    chore: operators * Arithmetic Operators * Assignment Operators

---
+++ May 22 2024/operators.py
@@ -0,0 +1,64 @@
+# Operators in Python
+
+# Arithmetic Operators
+num1 = 10
+num2 = 20
+
+# Addition
+print("Addition: ", num1 + num2)
+
+# Subtraction
+print("Subtraction: ", num1 - num2)
+
+# Multiplication
+print("Multiplication: ", num1 * num2)
+
+# Division
+print("Division: ", num1 / num2)
+
+# Modulus
+print("Modulus: ", num1 % num2)
+
+# Exponent
+print("Exponent: ", num1 ** num2)
+
+# Floor Division
+print("Floor Division: ", num1 // num2)
+
+# Output
+'''
+Addition:  30
+Subtraction:  -10
+Multiplication:  200
+Division:  0.5
+Modulus:  10
+Exponent:  100000000000000000000
+Floor Division:  0
+'''
+
+# Assignment Operators
+
+z = 10
+z += 5
+
+print("z = ", z)  # z = 15
+
+z -= 5
+print("z = ", z)  # z = 10
+
+z *= 5
+print("z = ", z)  # z = 50
+
+z /= 5
+print("z = ", z)  # z = 10.0
+
+z %= 5
+print("z = ", z)  # z = 0.0
+
+z **= 5
+print("z = ", z)  # z = 0.0
+
+z //= 5
+print("z = ", z)  # z = 0.0
+
+
