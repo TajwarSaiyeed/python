9e494fed87dcfce888544fe45d8b95ce6e436ec0
tajwarsaiyeed <rjabid36@gmail.com>
2024-07-07 05:39:44 +0600

    reformat: Jun 19 2024

--- Jun 19 2024/user_input.py
+++ Jun 19 2024/user_input.py
@@ -4,9 +4,8 @@ print(name)
 # # write a program to take number from the user as an input and add 6 to it
 
 num1 = int(input("Enter a number : "))
-print(type(num1)) # <class 'int'>
+print(type(num1))  # <class 'int'>
 print(num1 + 6)
-
 
 a = '''abid
     tajwar saiyeeed
