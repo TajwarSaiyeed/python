def0626b732c011603ca60180ff1de91ffa0c38d
tajwarsaiyeed <rjabid36@gmail.com>
2024-07-07 06:10:35 +0600

    feat: set & set methods

---
+++ Jul 07 2024/set&set_methods.py
@@ -0,0 +1,10 @@
+a = {3, 5, 23, 2, 4, 5, 3}
+a.add(100)
+# a.pop()
+# a2 = a.copy()
+a2 = {3, 200, 2, 5, 457, 457}
+# print(a.union(a2))
+print(a.intersection(a2))
+# a.clear()
+print(a)
+# print(a, a2)
